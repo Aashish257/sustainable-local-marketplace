de3b4de1b938e262d67bc45a089e1327bde0a59e
Aashish Shilkande <83578134+Aashish257@users.noreply.github.com>
2025-05-21 06:35:11 +0000

    delete files

--- Frontend/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Welcome to FarmNest</h1>
-      <p>Connecting local farmers with consumers</p>
-    </div>
-  );
-}
